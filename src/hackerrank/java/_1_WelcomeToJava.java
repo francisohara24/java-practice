006493cf3b2ce61cddc3554be60a0945265befee
francisohara24 <franciskohara@gmail.com>
2023-06-24 11:25:40 +0000

    Update _1_WelcomeToJava.java [final]

--- src/hackerrank/java/_1_WelcomeToJava.java
+++ src/hackerrank/java/_1_WelcomeToJava.java
@@ -1,10 +1,10 @@
+// Link: https://www.hackerrank.com/challenges/welcome-to-java/problem
 package hackerrank.java;
-/*
-Link: https://www.hackerrank.com/challenges/welcome-to-java/problem
-Task:
-*/
+
 public class _1_WelcomeToJava {
     public static void main(String[] args) {
         /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
     }
 }
