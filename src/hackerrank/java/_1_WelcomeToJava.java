149adbb10ba877abbbe2f6a1c55ce09a18e90db4
francisohara24 <franciskohara@gmail.com>
2023-06-24 00:00:01 +0000

    Add java: _1_WelcomeToJava.java

---
+++ src/hackerrank/java/_1_WelcomeToJava.java
@@ -0,0 +1,10 @@
+package hackerrank.java;
+/*
+Link: https://www.hackerrank.com/challenges/welcome-to-java/problem
+Task:
+*/
+public class _1_WelcomeToJava {
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
