f44a13fe8b1fce2bbffeee25777c19b1f95bdcbe
francisohara24 <franciskohara@gmail.com>
2024-01-01 20:03:54 -0500

    Feat: add _0_packaging package for practicing how the package system works in Java.

---
+++ src/practice/_0_packaging/ExampleClass.java
@@ -0,0 +1,15 @@
+package practice._0_packaging;
+
+public class ExampleClass{
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+}
+}
+
+/*
+Packaging in Java
+  - Java uses the package statements at the beginning of each source file to create its packaging system.
+  - Any directory could be a Java package as long as it contains java source files which in turn contain package statements
+ 	that specify that the directory the source files are contained in is a package.
+*/
