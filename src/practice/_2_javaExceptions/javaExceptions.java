443a08158df4df2982d9a08dbcaf36e4041e1970
francisohara24 <franciskohara@gmail.com>
2024-02-24 02:17:25 -0500

    Feat: add javaExceptions.java

---
+++ src/practice/_2_javaExceptions/javaExceptions.java
@@ -0,0 +1,24 @@
+/* Author: Francis O'Hara
+ * Date: 02-24-2024
+ * Description: Test how to throw simple exceptions in java
+ * */
+
+package practice._2_javaExceptions;
+
+public class javaExceptions {
+    public static void main (String[] args) {
+        // print hello
+        System.out.println("Hello World!");
+
+        // throw a simple exception
+        try {
+            throw new Exception("This is the string message associated with the Exception!");
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+
+        // print goodbye
+        System.out.println("Goodbye World!"); // ideally shouldn't run due to exception.
+    }
+}
