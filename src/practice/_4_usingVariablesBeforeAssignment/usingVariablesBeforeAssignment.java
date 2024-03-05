04b928620b4dbb7bbeb0ec100a952a61b8cced96
francisohara24 <franciskohara@gmail.com>
2024-03-05 10:45:09 -0500

    Feat: add practice 4 on testing variable usage without assignment.

---
+++ src/practice/_4_usingVariablesBeforeAssignment/usingVariablesBeforeAssignment.java
@@ -0,0 +1,27 @@
+/*
+File Name:   usingVariablesBeforeAssignment.java
+Author:      Francis O'Hara
+Date:        3/05/2024
+Description: Testing how variables behave when declared without assigning a value in Java.
+How to Run:  java -ea usingVariablesBeforeAssignment
+*/
+package practice._4_usingVariablesBeforeAssignment;
+
+public class usingVariablesBeforeAssignment {
+    public static void main(String[] args){
+        // declare int and String variables
+        int var1;
+        String var2;
+
+        // print variables without assignment
+        System.out.println(var1);
+        System.out.println(var2);
+
+        // RESULTS
+        /* build fails: variable might not have been initialized.
+           Fails to compile but JShell console outputs `0` for integer and `null` for float.
+        */
+
+
+    }
+}
