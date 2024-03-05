1a9f3dffe649930968951174bc25c8d04059d6dc
francisohara24 <franciskohara@gmail.com>
2024-03-05 18:09:10 -0500

    Chore: rename classname with PascalCase.

--- src/practice/_2_javaExceptions/JavaExceptions.java
+++ src/practice/_2_javaExceptions/JavaExceptions.java
@@ -5,7 +5,7 @@
 
 package practice._2_javaExceptions;
 
-public class javaExceptions {
+public class JavaExceptions {
     public static void main (String[] args) {
         // print hello
         System.out.println("Hello World!");
