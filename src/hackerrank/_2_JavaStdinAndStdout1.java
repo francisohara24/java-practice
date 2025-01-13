7d35cf80180f3bb499dc6771850666a46abcac00
francisohara24 <franciskohara@gmail.com>
2025-01-12 23:21:39 -0500

    feat: add java solution for hackerrank/java-stdin-stdout-1

---
+++ src/hackerrank/_2_JavaStdinAndStdout1.java
@@ -0,0 +1,15 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class _2_JavaStdinAndStdout1 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        System.out.println(a + "\n" + b + "\n" + c);
+    }
+}
