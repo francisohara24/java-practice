628b8d0f86d954b57561754cb26b7f55ca2d2c20
francisohara24 <franciskohara@gmail.com>
2025-01-17 23:20:18 -0500

    feat: add java solution for hackerrank/java-datatypes

---
+++ src/hackerrank/_9_JavaDataTypes.java
@@ -0,0 +1,52 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class _9_JavaDataTypes {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < n; i ++) {
+            boolean[] supportedTypes = {true, true, true, true};
+            String number = scanner.nextLine();
+            try {
+                byte result = Byte.parseByte(number);
+            }
+            catch(IllegalArgumentException e){
+                supportedTypes[0] = false;
+            }
+            try {
+                short result = Short.parseShort(number);
+            }
+            catch(IllegalArgumentException e){
+                supportedTypes[1] = false;
+            }
+            try{
+                int result = Integer.parseInt(number);
+            }
+            catch(IllegalArgumentException e){
+                supportedTypes[2] = false;
+            }
+            try{
+                long result = Long.parseLong(number);
+            }
+            catch(IllegalArgumentException e){
+                supportedTypes[3] = false;
+            }
+
+            if (supportedTypes[3]){
+                System.out.println(number + " can be fitted in:");
+               if (supportedTypes[0])
+                   System.out.println("* byte");
+               if (supportedTypes[1])
+                   System.out.println("* short");
+               if (supportedTypes[2])
+                   System.out.println("* int");
+               System.out.println("* long");
+            }
+            else
+                System.out.println(number + " can't be fitted anywhere.");
+        }
+    }
+}
