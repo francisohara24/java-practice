65f33198363a473ac4bbdb52731ade09e786d532
francisohara24 <franciskohara@gmail.com>
2025-01-29 00:15:25 -0500

    feat: add final java solution for hackerrank/java-string-reverse

--- src/hackerrank/_17_JavaStringReverse.java
+++ src/hackerrank/_17_JavaStringReverse.java
@@ -6,6 +6,16 @@ import java.util.Scanner;
 
 public class _17_JavaStringReverse {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.next();
+        String result = "Yes";
 
+        for (int i = 0; i < (word.length() / 2); i ++){
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)){
+                result = "No";
+                break;
+            }
+        }
+        System.out.println(result);
     }
 }
