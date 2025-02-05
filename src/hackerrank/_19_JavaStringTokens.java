36536271555a8695453c6996319629a903f635e5
francisohara24 <franciskohara@gmail.com>
2025-02-04 20:46:02 -0500

    feat: update java solution for hackerrank/java-string-tokens

--- src/hackerrank/_19_JavaStringTokens.java
+++ src/hackerrank/_19_JavaStringTokens.java
@@ -7,17 +7,29 @@ How to Run: java _19_JavaStringTokens
 */
 package hackerrank;
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class _19_JavaStringTokens {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
+        s.trim();
 
         String[] tokens = s.split("[^A-Za-z]+");
 
-        System.out.println(tokens.length);
+        List<String> final_tokens = new ArrayList<>();
 
         for (String token: tokens){
+            if (!token.isEmpty()){
+                final_tokens.add(token);
+            }
+        }
+
+        System.out.println(final_tokens.size());
+
+        for (String token: final_tokens){
             System.out.println(token);
         }
     }
